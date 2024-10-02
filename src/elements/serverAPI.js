d118f7b5795a0964f4def5e97ef84555b85a88ea
Mironov Egor <82310399+Valet-V0ult-de-Fur1e@users.noreply.github.com>
2024-10-02 19:26:20 +0300

    Update serverAPI.js

--- src/elements/serverAPI.js
+++ src/elements/serverAPI.js
@@ -1,4 +1,4 @@
-const getServerAPIURL = (securityFlag=false) => {
+const getServerAPIURL = (securityFlag=true) => {
     return securityFlag ? "https://195.133.198.89:8000" : "http://195.133.198.89:8000"
 }
 
