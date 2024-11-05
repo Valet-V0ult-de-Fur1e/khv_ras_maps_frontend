3efa62b907528fb2139cb41bca3f54f866c31abc
Valet-V0ult-de-Fur1e <82310399+Valet-V0ult-de-Fur1e@users.noreply.github.com>
2024-11-05 03:48:02 +0300

    fixed map legend

--- src/elements/mapLegend/mapLegend.js
+++ src/elements/mapLegend/mapLegend.js
@@ -1,21 +1,80 @@
 import "./styles.css";
 import { MapControl, useLeaflet, withLeaflet } from "react-leaflet";
 import L from "leaflet";
+import { useEffect } from "react";
+class Legend extends MapControl {
+  createLeafletElement(props) {
+    console.log(props)
+    this.crop_list = props.cropList
+    const MapLegend = L.Control.extend({
+      onAdd: (map) => {
+        const div = L.DomUtil.create("div", "info legend");
+        let labels = [];
+        this.crop_list.map(
+          (item) => {
+            labels.push(
+              '<i style="background:' + item.crop_color + '"></i> ' + item.crop_name
+            );
+          }
+        )
+        div.innerHTML = labels.join("<br>");
+        return div;
+      }
+    })
+    return new MapLegend({ position: 'bottomright' });
+  }
+  //   createLeafletElement(opts) {
+  //     const MapInfo = L.Control.extend({
+  //       onAdd: (map) => {
+  //         this.panelDiv = L.DomUtil.create('div', 'info');
+  //         return this.panelDiv;
+  //       }
+  //     });
+  //     return new MapInfo({ position: 'bottomleft' });
+  // }
 
-// class Legend extends MapControl {
-//   createLeafletElement(props) {
-//     console.log(props)
-//     this.crop_list = props.cropList
-//   }
+  // componentDidMount() {
+  //   const legend = L.control({ position: "bottomright" });
+  //   legend.onAdd = () => {
+  //     const div = L.DomUtil.create("div", "info legend");
+  //     let labels = [];
+  //     let data = [];
 
-//   componentDidMount() {
+  //     this.crop_list.map(
+  //       (item) => {
+  //         labels.push(
+  //           '<i style="background:' + item.crop_color + '"></i> ' + item.crop_name
+  //         );
+  //       }
+  //     )
+  //     div.innerHTML = labels.join("<br>");
+  //     return div;
+  //   };
+
+  //   const { map } = this.props.leaflet;
+  //   legend.addTo(map);
+  //   map.removeControl(legend);
+  //   // return () => {
+  //   //   // legend.removeFrom(map)
+  //   //   map.removeControl(legend);
+  //   // };
+  // }
+
+  // map.removeControl(legend);
+}
+
+export default withLeaflet(Legend);
+
+// const Legend = (props) => {
+//   const mapL = useLeaflet();
+
+//   useEffect(() => {
 //     const legend = L.control({ position: "bottomright" });
+//     console.log(props)
+//     const div = L.DomUtil.create("div", "info legend");
+//     let labels = [];
 //     legend.onAdd = () => {
-//       const div = L.DomUtil.create("div", "info legend");
-//       let labels = [];
-//       let data = [];
-
-//       this.crop_list.map(
+//       props.cropList.map(
 //         (item) => {
 //           labels.push(
 //             '<i style="background:' + item.crop_color + '"></i> ' + item.crop_name
@@ -25,44 +84,13 @@ import L from "leaflet";
 //       div.innerHTML = labels.join("<br>");
 //       return div;
 //     };
-
-//     const { map } = this.props.leaflet;
-//     legend.addTo(map);
+//     legend.addTo(mapL);
 //     return () => {
-//       map.removeControl(legend);
+//       mapL.removeControl(legend);
 //     };
-//   }
+//   }, [mapL]);
 
-//   // map.removeControl(legend);
-// }
+//   return null;
+// };
 
 // export default withLeaflet(Legend);
-
-const Legend = (crop_list) => {
-  const map = useLeaflet();
-
-  useEffect(() => {
-    const legend = L.control({ position: "bottomright" });
-    legend.onAdd = () => {
-      const div = L.DomUtil.create("div", "info legend");
-      let labels = [];
-      crop_list.map(
-        (item) => {
-          labels.push(
-            '<i style="background:' + item.crop_color + '"></i> ' + item.crop_name
-          );
-        }
-      )
-      div.innerHTML = labels.join("<br>");
-      return div;
-    };
-    legend.addTo(map);
-    return () => {
-      map.removeControl(legend);
-    };
-  }, [map]);
-
-  return null;
-};
-
-export default Legend;
